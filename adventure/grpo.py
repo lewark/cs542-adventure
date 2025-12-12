25e2e5dd7d581a4fe5ecc1710f6e14619602663b
Everett Lewark <78086132+lewark@users.noreply.github.com>
2025-12-12 13:13:30 -0700

    Attempt to use IterableDataset for GRPO
    This fails due to missing support in TRL

--- adventure/grpo.py
+++ adventure/grpo.py
@@ -1,35 +1,56 @@
 import jericho
-from datasets import Dataset
+from datasets import Dataset, IterableDataset
 
 from .model import load_model, save_model
 
 
 def make_grpo_dataset(env: jericho.FrotzEnv):
     states = {}
+    next_actions = []
     
     initial_obs, info = env.reset()
-    walkthrough = env.get_walkthrough()
+    initial_hash = env.get_world_state_hash()
+    #walkthrough = env.get_walkthrough()
 
     prompts = []
     hashes = []
+
+    max_prompts = 10
     
-    obs = initial_obs
-    for step in walkthrough:
-        prompts.append([{"role": "user", "content": obs}])
-        
-        state_hash = env.get_world_state_hash()
-        hashes.append(state_hash)
-        if state_hash not in states:
-            states[state_hash] = env.get_state()
-        
-        obs, reward, done, info = env.step(step)
+    def generate_dataset():
+        obs = initial_obs
+        #state_hash = env.get_world_state_hash()
+        state_hash = initial_hash
+        prompts.append({"role": "user", "content": obs})
 
-        if done:
-            break
+        print("Yielding first result")
 
-    env.close()
+        last_result = {"prompt": prompts, "state_hash": state_hash}
+        yield last_result
 
-    return Dataset.from_dict({"prompt": prompts, "state_hashes": hashes}), states
+        print("Yielded first result")
+
+        while True:
+            if len(next_actions) == 0:
+                print("Warning: Next action list is empty")
+                yield last_result
+                continue
+
+            state_hash, action, obs = next_actions.pop(0)
+            
+            prompts.append({"role": "assistant", "content": action})
+            prompts.append({"role": "user", "content": obs})
+
+            if len(prompts) > max_prompts:
+                prompts.pop(0)
+                prompts.pop(0)
+
+            last_result = {"prompt": prompts, "state_hash": state_hash}
+            yield last_result
+
+        #env.close()
+
+    return IterableDataset.from_generator(generate_dataset), states, next_actions
 
 
 def shorten_response(response: str):
@@ -39,17 +60,25 @@ def shorten_response(response: str):
 # Workaround for segfault in FrotzEnv.set_state
 # Create + reuse a separate env within the GRPO reward function
 reward_env = None
+global_var = None
 
-def get_reward_func(game_file: str):
+def get_reward_func(game_file: str, states: dict, next_actions: list):
 
     def reward_func(prompts, completions, state_hashes, **kwargs): #(prompts, completions, **kwargs):
         global reward_env
+        global global_var
+        global_var = "hello"
         #env.set_state(states[state_hash])
         #env.step
         if reward_env is None:
             reward_env = jericho.FrotzEnv(game_file)
         #local_env = env.copy()
         scores = []
+        best_score = None
+        best_action = None
+        best_state = None
+        best_hash = None
+        best_obs = None
         for prompt, completion, state_hash in zip(prompts, completions, state_hashes):
             reward_env.set_state(states[state_hash])
             command = completion[0]["content"]
@@ -57,8 +86,9 @@ def get_reward_func(game_file: str):
             cur_inv_size = len(reward_env.get_inventory())
             obs, reward, done, info = reward_env.step(command)
             new_inv_size = len(reward_env.get_inventory())
+            new_hash = reward_env.get_world_state_hash()
 
-            if reward_env.get_world_state_hash() == state_hash:
+            if new_hash == state_hash:
                 # Punish taking an invalid action
                 reward -= 1.0
             #if reward_env.get_world_state_hash() != state_hash:
@@ -73,8 +103,18 @@ def get_reward_func(game_file: str):
             #print(f"'{short_desc}': '{command}' -> '{short_obs}' {reward}")
             #print(info)
 
+            if best_score is None or reward > best_score:
+                best_score = reward
+                best_action = command
+                best_state = reward_env.get_state()
+                best_hash = new_hash
+                best_obs = obs
+
             scores.append(reward)
         #local_env.close()
+
+        states[best_hash] = best_state
+        next_actions.append((best_hash, best_action, best_obs))
 
         #print(scores)
         return scores
@@ -108,11 +148,12 @@ def make_grpo_trainer(model, tokenizer, grpo_dataset, reward_func):
         num_generations = 4, # Decrease if out of memory
         max_prompt_length = max_prompt_length,
         max_completion_length = max_seq_length - max_prompt_length,
-        max_steps = 500,
+        max_steps = 5,
         save_steps = 250,
         max_grad_norm = 1.0,
         report_to = "none",
         output_dir = "outputs",
+        dispatch_batches = False
     )
 
     # https://csolab.research.google.com/github/unslothai/notebooks/blob/main/nb/Advanced_Llama3_2_(3B)_GRPO_LoRA.ipynb
@@ -135,10 +176,11 @@ if __name__ == "__main__":
     env_file = "./z-machine-games-master/jericho-game-suite/zork1.z5"
     
     env = jericho.FrotzEnv(env_file)
-    grpo_dataset, states = make_grpo_dataset(env)
+    grpo_dataset, states, next_actions = make_grpo_dataset(env)
     env.close()
 
-    reward_func = get_reward_func(env_file)
+    reward_func = get_reward_func(env_file, states, next_actions)
     grpo_trainer = make_grpo_trainer(model, tokenizer, grpo_dataset, reward_func)
     grpo_trainer.train()
-    save_model(model, tokenizer, "grpo_model")
+    print(global_var)
+    save_model(model, tokenizer, "grpo_model_2")
